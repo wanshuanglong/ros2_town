dae8bdf1f838674c602af9a50db6a4ad3bc352e4
wandong <wanshuanglong@mychery.com>
2023-12-04 10:05:21 +0800

    试用launch文件启动三个节点

--- src/village_li/launch/village.launch.py
+++ src/village_li/launch/village.launch.py
@@ -7,14 +7,23 @@ def generate_launch_description():
     # 创建Actions.Node对象li_node，标明李四所在位置
     li4_node = Node(
         package="village_li",
-        executable="li4_node"
+        executable="li4_node",
+        output = 'screen',
+        parameters=[{'write_timer_period':3}]
         )
     # 创建Actions.Node对象wang2_node，标明王二所在位置
     wang2_node = Node(
         package="village_wang",
-        executable="wang2_node"
+        executable="wang2_node",
+        output = 'screen',
+        parameters=[{'novel_price':2}]
         )
+    # 创建Actions.Node对象zhang3_node，标明王二所在位置
+    zhang3_node = Node(
+        package="village_zhang",
+        executable="zhang3_node"
+    )
     # 创建LaunchDescription对象launch_description,用于描述launch文件
-    launch_description = LaunchDescription([li4_node,wang2_node])
+    launch_description = LaunchDescription([li4_node,wang2_node,zhang3_node])
     # 返回让ROS2根据launch描述执行节点
     return launch_description
